6568e2e33120a95b458574538e9bfd1c4158e45c
sciencedude200 <77691770+sciencedude200@users.noreply.github.com>
2021-01-19 18:09:42 +0000

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,9 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+
+}
+// Auto-generated code. Do not edit.
